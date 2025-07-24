8149638e6fb9d764b5a9659bf46701061282e379
nikolat27 <samalizadeh899@gmail.com>
2025-07-24 17:59:53 +0330

    Addded mock handlers + boilterplate

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,8 +1,25 @@
 package handlers
 
+import (
+	"chat_app/database/models"
+	"chat_app/paseto"
+)
+
 type Handler struct {
+	Models *models.Models
+	Paseto *paseto.Maker
 }
 
-func New() *Handler {
-	return &Handler{}
+func New(models *models.Models) (*Handler, error) {
+	pasetoInstance, err := paseto.New()
+	if err != nil {
+		return nil, err
+	}
+
+	var handler = &Handler{
+		Models: models,
+		Paseto: pasetoInstance,
+	}
+
+	return handler, nil
 }
