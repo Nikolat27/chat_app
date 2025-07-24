53a0feaab91040e18fd8b512c0993f04a2e7af21
nikolat27 <samalizadeh899@gmail.com>
2025-07-24 18:00:04 +0330

    Added database + handler initializing

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,46 @@
 package main
 
 import (
+	"chat_app/database"
+	"chat_app/database/models"
 	"chat_app/handlers"
 	"chat_app/server"
+	"errors"
 	"log/slog"
 	"os"
 )
 
 func main() {
-	port := getPort()
+	uri, err := getMongoURI()
+	if err != nil {
+		panic(err)
+	}
 
-	handlerInstance := handlers.New()
+	db, err := database.New(uri)
+	if err != nil {
+		panic(err)
+	}
 
-	srv := server.New(port, handlerInstance)
+	newModels := models.New(db)
+
+	handlerInstance := handlers.New(newModels)
+
+	srv := server.New(getPort(), handlerInstance)
 	defer srv.Close()
 
 	if err := srv.Run(); err != nil {
 		slog.Error("running http server", "error", err)
 		return
 	}
+}
+
+func getMongoURI() (string, error) {
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		return "", errors.New("MONGO_URI env variable does not exist")
+	}
+
+	return uri, nil
 }
 
 func getPort() string {
