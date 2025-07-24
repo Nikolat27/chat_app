3e7a167de3b7366ef9489f458ffc408031863851
nikolat27 <samalizadeh899@gmail.com>
2025-07-24 19:24:48 +0330

    Added user registering

--- database/models/users.go
+++ database/models/users.go
@@ -1,9 +1,25 @@
 package models
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
 
 type UserModel struct {
 	collection *mongo.Collection
+}
+
+type User struct {
+	Id             primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Username       string             `json:"username" bson:"username"`
+	Email          string             `json:"email" bson:"email"`
+	HashedPassword string             `json:"hashed_password" bson:"hashed_password"`
+	Salt           string             `json:"salt" bson:"salt"`
+	CreatedAt      time.Time          `json:"created_at" bson:"created_at"`
 }
 
 func NewUserModel(db *mongo.Database) *UserModel {
@@ -12,5 +28,38 @@ func NewUserModel(db *mongo.Database) *UserModel {
 	}
 }
 
-func (user *UserModel) Create() {
+func (user *UserModel) Create(username, email, hashedPassword, salt string) (primitive.ObjectID, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var newUser = &User{
+		Username:       username,
+		Email:          email,
+		HashedPassword: hashedPassword,
+		Salt:           salt,
+		CreatedAt:      time.Now(),
+	}
+
+	result, err := user.collection.InsertOne(ctx, newUser)
+	if err != nil {
+		return primitive.NilObjectID, err
+	}
+
+	return result.InsertedID.(primitive.ObjectID), nil
+}
+
+// Get -> Returns One
+func (user *UserModel) Get(filter bson.M, projection bson.M) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	findOptions := options.FindOne()
+	findOptions.SetProjection(projection)
+
+	var userInstance User
+	if err := user.collection.FindOne(ctx, filter, findOptions).Decode(&userInstance); err != nil {
+		return nil, err
+	}
+
+	return &userInstance, nil
 }
--- handlers/auth.go
+++ handlers/auth.go
@@ -2,11 +2,57 @@ package handlers
 
 import (
 	"chat_app/utils"
+	"encoding/hex"
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 )
 
 func (handler *Handler) Register(w http.ResponseWriter, r *http.Request) {
-	utils.WriteJSON(w, http.StatusOK, "register")
+	var input struct {
+		Username    string `json:"username"`
+		Email       string `json:"email"`
+		RawPassword string `json:"password"`
+	}
+
+	if err := utils.ParseJSON(r.Body, 1_000, &input); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, "parseJson", err)
+		return
+	}
+
+	filter := bson.M{
+		"username": input.Username,
+		"email":    input.Email,
+	}
+
+	projection := bson.M{
+		"_id": 1,
+	}
+
+	_, err := handler.Models.User.Get(filter, projection)
+	if err != nil && !errors.Is(err, mongo.ErrNoDocuments) {
+		utils.WriteError(w, http.StatusBadRequest, "getUserInstance", "either this username or email exists")
+		return
+	}
+
+	salt, err := utils.GenerateSalt(14)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, "generatingSalt", err)
+		return
+	}
+
+	hashedPassword := utils.Hash(input.RawPassword, salt)
+
+	encodedHashedPassword := hex.EncodeToString([]byte(hashedPassword))
+	encodedSalt := hex.EncodeToString([]byte(salt))
+
+	if _, err := handler.Models.User.Create(input.Username, input.Email, encodedHashedPassword, encodedSalt); err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, "creatingUserInstance", err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, "user registered successfully")
 }
 
 func (handler *Handler) Login(w http.ResponseWriter, r *http.Request) {
