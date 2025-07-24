e1e0f628ca17419c13ea2235d8ac2eaee236a3d9
nikolat27 <samalizadeh899@gmail.com>
2025-07-25 01:08:05 +0330

    Added registering and logging in

--- handlers/auth.go
+++ handlers/auth.go
@@ -7,6 +7,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
+	"time"
 )
 
 func (handler *Handler) Register(w http.ResponseWriter, r *http.Request) {
@@ -30,9 +31,8 @@ func (handler *Handler) Register(w http.ResponseWriter, r *http.Request) {
 		"_id": 1,
 	}
 
-	_, err := handler.Models.User.Get(filter, projection)
-	if err != nil && !errors.Is(err, mongo.ErrNoDocuments) {
-		utils.WriteError(w, http.StatusBadRequest, "getUserInstance", "either this username or email exists")
+	if _, err := handler.Models.User.Get(filter, projection); err == nil {
+		utils.WriteError(w, http.StatusBadRequest, "usernameEmailTaken", "user with this email or username exists already")
 		return
 	}
 
@@ -42,9 +42,9 @@ func (handler *Handler) Register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	hashedPassword := utils.Hash(input.RawPassword, salt)
+	hashedPassword := utils.Hash([]byte(input.RawPassword), []byte(salt))
 
-	encodedHashedPassword := hex.EncodeToString([]byte(hashedPassword))
+	encodedHashedPassword := hex.EncodeToString(hashedPassword[:])
 	encodedSalt := hex.EncodeToString([]byte(salt))
 
 	if _, err := handler.Models.User.Create(input.Username, input.Email, encodedHashedPassword, encodedSalt); err != nil {
@@ -56,5 +56,63 @@ func (handler *Handler) Register(w http.ResponseWriter, r *http.Request) {
 }
 
 func (handler *Handler) Login(w http.ResponseWriter, r *http.Request) {
-	utils.WriteJSON(w, http.StatusBadRequest, "login")
+	var input struct {
+		Username    string `json:"username"`
+		RawPassword string `json:"password"`
+	}
+
+	if err := utils.ParseJSON(r.Body, 1_000, &input); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, "parsingJson", err)
+		return
+	}
+
+	filter := bson.M{
+		"username": input.Username,
+	}
+
+	projection := bson.M{
+		"_id":             1,
+		"hashed_password": 1,
+		"salt":            1,
+	}
+
+	user, err := handler.Models.User.Get(filter, projection)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			utils.WriteError(w, http.StatusBadRequest, "userDoesNotExist", "user with this username does not exist")
+			return
+		}
+
+		utils.WriteError(w, http.StatusInternalServerError, "userGetError", "failed to fetch user")
+		return
+	}
+
+	decodedHashedPassword, err := hex.DecodeString(user.HashedPassword)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, "decodeHashedPassword", err)
+		return
+	}
+
+	decodedSalt, err := hex.DecodeString(user.Salt)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, "decodeSalt", err)
+		return
+	}
+
+	if !utils.VerifyHash(decodedHashedPassword, decodedSalt, []byte(input.RawPassword)) {
+		utils.WriteError(w, http.StatusBadRequest, "passwordValidation", "password is invalid")
+		return
+	}
+
+	token, err := handler.Paseto.CreateToken(user.Id.Hex(), input.Username, 12*time.Hour)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, "createPasetoToken", "failed to create paseto token")
+		return
+	}
+
+	var response = map[string]string{
+		"token": token,
+	}
+
+	utils.WriteJSON(w, http.StatusOK, response)
 }
