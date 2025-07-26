c014a1c14ae5f953acbc1737f4c1c49f2cc47024
nikolat27 <samalizadeh899@gmail.com>
2025-07-26 22:01:52 +0330

    Added new handlers

--- backend/handlers/users.go
+++ backend/handlers/users.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 )
@@ -31,6 +32,31 @@ func (handler *Handler) CreateUser(username string, rawPassword []byte) *utils.E
 	}
 
 	return nil
+}
+
+func (handler *Handler) SearchUser(w http.ResponseWriter, r *http.Request) {
+	if _, errResp := utils.CheckAuth(r.Header, handler.Paseto); errResp != nil {
+		utils.WriteError(w, http.StatusBadRequest, errResp.Type, errResp.Detail)
+		return
+	}
+
+	query := r.URL.Query().Get("q")
+	if query == "" {
+		utils.WriteError(w, http.StatusBadRequest, "missingQuery", "q query is missing")
+		return
+	}
+
+	filter := bson.M{
+		"username": query,
+	}
+
+	userInstance, err := handler.Models.User.Get(filter, bson.M{})
+	if err != nil && !errors.Is(err, mongo.ErrNoDocuments) {
+		utils.WriteError(w, http.StatusBadRequest, "getUser", err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, userInstance)
 }
 
 func (handler *Handler) UploadAvatar(w http.ResponseWriter, r *http.Request) {
@@ -99,4 +125,87 @@ func (handler *Handler) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	utils.WriteJSON(w, http.StatusOK, "user deleted successfully")
+}
+
+func (handler *Handler) GetUserChats(w http.ResponseWriter, r *http.Request) {
+	payload, errResp := utils.CheckAuth(r.Header, handler.Paseto)
+	if errResp != nil {
+		utils.WriteError(w, http.StatusBadRequest, errResp.Type, errResp.Detail)
+		return
+	}
+
+	filter := bson.M{
+		"participants": bson.M{
+			"$in": []primitive.ObjectID{payload.UserId},
+		},
+	}
+
+	chats, err := handler.Models.Chat.GetAll(filter, bson.M{}, 1, 10)
+	if err != nil && !errors.Is(err, mongo.ErrNoDocuments) {
+		utils.WriteError(w, http.StatusBadRequest, "getChats", err)
+		return
+	}
+
+	avatarUrls := make(map[string]string)
+	usernames := make(map[string]string)
+	for _, chat := range chats {
+		otherUserId := getOtherUserId(chat.Participants, payload.UserId)
+		url, _ := getUserAvatarUrl(otherUserId, handler)
+		username, _ := getUserUsername(otherUserId, handler)
+
+		avatarUrls[chat.Id.Hex()] = url
+		usernames[chat.Id.Hex()] = username
+	}
+
+	response := map[string]any{
+		"chats":       chats,
+		"avatar_urls": avatarUrls,
+		"usernames":   usernames,
+	}
+
+	utils.WriteJSON(w, http.StatusOK, response)
+}
+
+func getOtherUserId(participants []primitive.ObjectID, userId primitive.ObjectID) primitive.ObjectID {
+	for _, participant := range participants {
+		if participant != userId {
+			return participant
+		}
+	}
+
+	return userId
+}
+
+func getUserAvatarUrl(id primitive.ObjectID, handler *Handler) (string, error) {
+	filter := bson.M{
+		"_id": id,
+	}
+
+	projection := bson.M{
+		"avatar_url": 1,
+	}
+
+	user, err := handler.Models.User.Get(filter, projection)
+	if err != nil {
+		return "", err
+	}
+
+	return user.AvatarUrl, nil
+}
+
+func getUserUsername(id primitive.ObjectID, handler *Handler) (string, error) {
+	filter := bson.M{
+		"_id": id,
+	}
+
+	projection := bson.M{
+		"username": 1,
+	}
+
+	user, err := handler.Models.User.Get(filter, projection)
+	if err != nil {
+		return "", err
+	}
+
+	return user.Username, nil
 }
