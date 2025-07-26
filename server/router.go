543a95d10a3314a68634bff106ea17ba3cc1a300
nikolat27 <samalizadeh899@gmail.com>
2025-07-26 10:48:21 +0330

    Added static file shower

--- server/router.go
+++ server/router.go
@@ -4,6 +4,7 @@ import (
 	"chat_app/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
 )
 
 type Router struct {
@@ -21,17 +22,21 @@ func NewRouter(handler *handlers.Handler) *Router {
 		r.Post("/auth/login", handler.Login)
 
 		r.Post("/chat/create", handler.CreateChat)
+		r.Post("/chat/upload/{chat_id}", handler.UploadChatImage)
 		r.Get("/chat/get/{chat_id}", handler.GetChatMessages)
 		r.Delete("/chat/delete/{chat_id}", handler.DeleteChat)
 
 		r.Get("/websocket/chat/add/{chat_id}", handler.AddChatWebsocket)
 
-		r.Put("/message/update/${message_id}", handler.EditMessage)
+		r.Put("/message/update/{message_id}", handler.EditMessage)
 		r.Delete("/message/delete/sender/{message_id}", handler.DeleteMessageForSender)
 		r.Delete("/message/delete/receiver/{message_id}", handler.DeleteMessageForReceiver)
 		r.Delete("/message/delete/all/{message_id}", handler.DeleteMessageForAll)
 
 	})
+
+	fs := http.FileServer(http.Dir("./uploads"))
+	routerInstance.Handle("/static/*", http.StripPrefix("/static/", fs))
 
 	return &Router{
 		CoreRouter: routerInstance,
