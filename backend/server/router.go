d9256fcc827b331f1d410bed1330f4e15d9d0e90
nikolat27 <samalizadeh899@gmail.com>
2025-07-27 11:38:57 +0330

    Modified the route for readability

--- backend/server/router.go
+++ backend/server/router.go
@@ -30,7 +30,7 @@ func NewRouter(handler *handlers.Handler) *Router {
 
 		r.Post("/chat/create", handler.CreateChat)
 		r.Post("/chat/upload/{chat_id}/{receiver_id}", handler.UploadChatImage)
-		r.Get("/chat/get/{chat_id}", handler.GetChatMessages)
+		r.Get("/chat/get/{chat_id}/messages", handler.GetChatMessages)
 		r.Delete("/chat/delete/{chat_id}", handler.DeleteChat)
 		// chat websocket
 		r.Get("/websocket/chat/add/{chat_id}", handler.AddChatWebsocket)
