5faf26789a93a3226160277f8b1a8ddc0e8c76b7
nikolat27 <samalizadeh899@gmail.com>
2025-07-29 16:46:14 +0330

    Added approvals routes + New comments

--- backend/server/router.go
+++ backend/server/router.go
@@ -20,15 +20,18 @@ func NewRouter(handler *handlers.Handler) *Router {
 	// prefix api
 	routerInstance.Route("/api", func(r chi.Router) {
 
+		// Authentication
 		r.Post("/register", handler.Register)
 		r.Post("/login", handler.Login)
 
+		// Users
 		r.Get("/user/search", handler.SearchUser)
 		r.Delete("/user/delete", handler.DeleteUser)
 		r.Post("/user/upload-avatar", handler.UploadAvatar)
 		r.Get("/user/get-chats", handler.GetUserChats)
 		r.Get("/user/get-secret-chats", handler.GetUserSecretChats)
 
+		// Chats
 		r.Post("/chat/create", handler.CreateChat)
 		r.Post("/chat/upload/{chat_id}/{receiver_id}", handler.UploadChatImage)
 		r.Get("/chat/get/{chat_id}/messages", handler.GetChatMessages)
@@ -36,10 +39,12 @@ func NewRouter(handler *handlers.Handler) *Router {
 		// chat websocket
 		r.Get("/websocket/chat/add/{chat_id}", handler.AddChatWebsocket)
 
+		// Messages
 		r.Put("/message/update/{message_id}", handler.EditMessage)
 		r.Delete("/message/delete/sender/{message_id}", handler.DeleteMessageForSender)
 		r.Delete("/message/delete/all/{message_id}", handler.DeleteMessageForAll)
 
+		// Groups
 		r.Post("/group/create", handler.CreateGroup)
 		r.Get("/group/join/{invite_link}", handler.JoinGroup)
 		r.Get("/group/get/{group_id}", handler.GetGroupMessages)
@@ -48,11 +53,13 @@ func NewRouter(handler *handlers.Handler) *Router {
 		// group websocket
 		r.Get("/websocket/group/add/{group_id}", handler.AddGroupWebsocket)
 
+		// Save Messages
 		r.Post("/save-message/create", handler.CreateSaveMessage)
 		r.Get("/save-message/get", handler.GetSaveMessages)
 		r.Put("/save-message/update/{message_id}", handler.EditSaveMessage)
 		r.Delete("/save-message/delete", handler.DeleteSaveMessage)
 
+		// Secret Chats
 		r.Get("/secret-chat/get/{secret_chat_id}", handler.GetSecretChat)
 		r.Post("/secret-chat/create", handler.CreateSecretChat)
 		r.Get("/secret-chat/get/{secret_chat_id}/messages", handler.GetSecretChatMessages)
@@ -62,6 +69,13 @@ func NewRouter(handler *handlers.Handler) *Router {
 		r.Post("/secret-chat/approve/{secret_chat_id}", handler.ApproveSecretChat)
 		// chat websocket
 		r.Get("/websocket/secret-chat/add/{secret_chat_id}", handler.AddSecretChatWebsocket)
+
+		// Approvals
+		r.Get("/received-approvals/get/", handler.GetReceivedApprovals)
+		r.Get("/sent-approvals/get/", handler.GetSentApprovals)
+		r.Post("/approvals/create/{group_id}", handler.CreateApproval)
+		r.Put("/approvals/edit-status", handler.EditApprovalStatus)
+		r.Delete("/approvals/delete/{approval_id}", handler.DeleteApproval)
 
 	})
 
