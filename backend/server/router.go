0bca2f584769bbf70b39e864947116819af4545e
nikolat27 <samalizadeh899@gmail.com>
2025-07-30 11:35:31 +0330

    Added new route

--- backend/server/router.go
+++ backend/server/router.go
@@ -49,7 +49,7 @@ func NewRouter(handler *handlers.Handler) *Router {
 		r.Post("/group/create", handler.CreateGroup)
 		r.Get("/group/join/{invite_link}", handler.JoinGroup)
 		r.Get("/group/get/{group_id}/messages", handler.GetGroupMessages)
-		r.Get("/group/get/{group_id}/users", handler.GetGroupMessages)
+		r.Get("/group/get/{group_id}/users", handler.GetGroupUsers)
 		r.Delete("/group/leave/{group_id}", handler.LeaveGroup)
 		r.Delete("/group/remove-user/{group_id}/{user_id}", handler.RemoveUserFromGroup)
 		r.Delete("/group/delete/{group_id}", handler.DeleteGroup)
